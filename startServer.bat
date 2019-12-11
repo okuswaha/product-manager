118c6c17d46a57145edfd1c8c12c028376d2c40a
okuswaha <kuswaha.omprakash1@gmail.com>
2019-12-10 19:51:00 -0500

    adding debug command to the start batch file

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,4 @@
 call mvn -Dmaven.test.skip clean install
 call java -jar target/product-manager-1.0-SNAPSHOT.jar db migrate configuration.yaml
-call java -jar target/product-manager-1.0-SNAPSHOT.jar server configuration.yaml
+:: call java -jar target/product-manager-1.0-SNAPSHOT.jar server configuration.yaml
+call java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7896 target/product-manager-1.0-SNAPSHOT.jar server configuration.yaml
